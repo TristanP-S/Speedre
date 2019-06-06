889b43b2fb4f56809877ba1ebb27f731d3d7f728
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2019-06-06 19:58:09 -0400

    Added APICommand file

---
+++ Speedre/Speedre/Others/APICommands.swift
@@ -0,0 +1,12 @@
+//
+//  APICommands.swift
+//  Speedre
+//
+//  Created by Tristan Pudell-Spatscheck on 6/6/19.
+//  Copyright © 2019 TAPS. All rights reserved.
+//
+
+import Foundation
+class APICommands{
+    
+}
